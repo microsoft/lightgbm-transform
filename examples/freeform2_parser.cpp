f6fe40b0bb7fb309c0aaa648f50c6a3852770c34
Jincheng Chen <chjinche@microsoft.com>
2021-11-07 15:50:35 +0000

    update freeform2 parser and add python tests

--- examples/freeform2_parser.cpp
+++ examples/freeform2_parser.cpp
@@ -3,6 +3,8 @@
 #include <boost/algorithm/string/split.hpp>
 #include <boost/algorithm/string.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
 #include "TransformProcessor.h"
 using namespace std;
 
@@ -11,25 +13,25 @@ namespace LightGBM {
 class FreeForm2Parser: public Parser {
   public:
     FreeForm2Parser(std::string config_str) {
-      string label_key = "labelId:";
-      string expr_key = "transform:\n";
-      string header_key = "header:\n";
-
-      size_t start_pos = config_str.find(label_key);
-      config_str.erase(0, start_pos);
-      size_t end_pos = config_str.find("\n");
-      string label_line = config_str.substr(label_key.size(), end_pos);
-      int label_idx = std::stod(label_line);
-
-      start_pos = config_str.find(expr_key);
-      config_str.erase(0, start_pos);
-      end_pos = config_str.find("end of transform");
-      string transform_str = config_str.substr(expr_key.size(), end_pos);
-
-      start_pos = config_str.find(header_key);
-      config_str.erase(0, start_pos);
-      end_pos = config_str.find("end of header");
-      string header_str = config_str.substr(header_key.size(), end_pos);
+      // config should follow json format.
+      std::stringstream config_ss(config_str);
+      boost::property_tree::ptree ptree;
+      boost::property_tree::read_json(config_ss, ptree);
+      int label_idx = -1;
+      auto label_node = ptree.get_child_optional("labelId");
+      if (label_node) {
+        label_idx = label_node->get_value<int>();
+      }
+      std::string transform_str = "";
+      auto transform_node = ptree.get_child_optional("transform");
+      if (transform_node) {
+        transform_str = transform_node->get_value<std::string>();
+      }
+      std::string header_str = "";
+      auto header_node = ptree.get_child_optional("header");
+      if (header_node) {
+        header_str = header_node->get_value<std::string>();
+      }
       Log::Info("Initializing transform processor.");
       transform_.reset(new TransformProcessor(transform_str, header_str, label_idx));
     }
