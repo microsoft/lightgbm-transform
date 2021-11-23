e398e73fcc7e2725f685c3d9888e1246c1f5c5f1
Jincheng Chen <chjinche@microsoft.com>
2021-11-23 08:49:36 +0000

    update component sample

--- azureml/run.py
+++ azureml/run.py
@@ -23,8 +23,14 @@ inference_data_name = 'test.tsv'
 parser_config_name = 'parser_config_file.json'
 datastore.upload(src_dir=data_dir, target_path=data_dir, overwrite=True, show_progress=True)
 training_set = Dataset.File.from_files(path=[(datastore, f'{data_dir}/{training_data_name}')])
+training_set.register(ws, name="train tsv")
+training_set = Dataset.get_by_name(ws, name="train tsv")
 inference_set = Dataset.File.from_files(path=[(datastore, f'{data_dir}/{inference_data_name}')])
+inference_set.register(ws, name="test tsv")
+inference_set = Dataset.get_by_name(ws, name="test tsv")
 parser_config = Dataset.File.from_files(path=[(datastore, f'{data_dir}/{parser_config_name}')])
+parser_config.register(ws, name="parser config file")
+parser_config = Dataset.get_by_name(ws, name="parser config file")
 
 # Create components
 train_component_func = Component.from_yaml(ws, yaml_file='components/train/train.yaml')
@@ -38,7 +44,8 @@ eval_component_func = Component.from_yaml(ws, yaml_file='components/evaluate/eva
               description='Test lightGBM feature transformation binding',
               default_compute_target=cluster_name)
 def test_pipeline(training_set, inference_set, parser_config):
-    train = train_component_func(train_data_path=training_set, parser_config_path=parser_config, label="m:Rating")
+    train = train_component_func(train_data_path=training_set, parser_config_path=parser_config,
+                                 label="m:Rating", label_gain=','.join([str(i) for i in range(350)]))
     train.comment = "Use FreeForm2 parser to do built-in feature transformation in training."
     infer = infer_component_func(infer_data_path=inference_set, model_path=train.outputs.model_path)
     infer.comment = "The model has saved parser info, will do the transform automatically, " \
