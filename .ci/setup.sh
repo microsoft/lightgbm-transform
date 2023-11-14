16063154bc3e6c9b789302c4ae7890cec178b1bc
Manisha Dhingra <madhin@microsoft.com>
2023-11-14 16:17:57 +0530

    fix

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -6,9 +6,9 @@ mkdir build
 # precompile mpi option.
 cd build && cmake ../ -DUSE_MPI=ON && make -j4 && cd ../ || exit -1
 # copy all shared libs to lightgbm python package directory.
-# cp ./lib_custom_parser.so ${lgb_python_pkg_dir}/lightgbm && \
-# cp ./src/lib_transform.so ${lgb_python_pkg_dir}/lightgbm && \
-# cp ./external_libs/LightGBM/lib_lightgbm.so ${lgb_python_pkg_dir}/lightgbm || exit -1
+cp ./lib_custom_parser.so ${lgb_python_pkg_dir}/ && \
+cp ./src/lib_transform.so ${lgb_python_pkg_dir}/ && \
+cp ./external_libs/LightGBM/lib_lightgbm.so ${lgb_python_pkg_dir}/ || exit -1
 # modify `basic.py` to load all libs first, or cannot find them when calling python interfaces.
 cp ${lgb_python_pkg_dir}/lightgbm/basic.py raw && cat ./scripts/load_precompiled_libs.py ${lgb_python_pkg_dir}/lightgbm/basic.py > tmp && cp tmp ${lgb_python_pkg_dir}/lightgbm/basic.py || exit -1
 # install python package
