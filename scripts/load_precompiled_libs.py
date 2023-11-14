daa1c641f8aba833ef2ed967c218adfc379a2c61
Manisha Dhingra <madhin@microsoft.com>
2023-11-14 14:19:57 +0530

    remove loading

--- scripts/load_precompiled_libs.py
+++ scripts/load_precompiled_libs.py
@@ -1,6 +1,6 @@
 import ctypes
 from pathlib import Path
 
-CUSTOM_PARSER_LIB_NAME = 'lib_custom_parser.so'
-for p in ['lib_transform.so', 'lib_lightgbm.so', CUSTOM_PARSER_LIB_NAME]:
-    ctypes.cdll.LoadLibrary(str(Path(__file__).resolve().parent / p))
+# CUSTOM_PARSER_LIB_NAME = 'lib_custom_parser.so'
+# for p in ['lib_transform.so', 'lib_lightgbm.so', CUSTOM_PARSER_LIB_NAME]:
+#     ctypes.cdll.LoadLibrary(str(Path(__file__).resolve().parent / p))
