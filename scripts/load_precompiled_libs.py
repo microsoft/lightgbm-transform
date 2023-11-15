9d5d0e3e107734fcdef4736cf50953eeeb0d86de
Manisha Dhingra <manishadhingra2007@gmail.com>
2023-11-15 13:49:54 +0530

    lightgbm version update (#26)
    * lightgbm version update to 4.1.0
    
    * update ci
    
    * remove loading
    
    * fix
    
    * copying so files to compile folder
    
    * change path of precompiled libs
    
    * change path
    
    ---------
    
    Co-authored-by: Manisha Dhingra <madhin@microsoft.com>

--- scripts/load_precompiled_libs.py
+++ scripts/load_precompiled_libs.py
@@ -3,4 +3,4 @@ from pathlib import Path
 
 CUSTOM_PARSER_LIB_NAME = 'lib_custom_parser.so'
 for p in ['lib_transform.so', 'lib_lightgbm.so', CUSTOM_PARSER_LIB_NAME]:
-    ctypes.cdll.LoadLibrary(str(Path(__file__).resolve().parent / p))
+    ctypes.cdll.LoadLibrary(str(Path(__file__).resolve().parents[1] / p))
